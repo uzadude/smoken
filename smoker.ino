ae69686985ff8aebf343bceacae8b93675f29367
Ohad Raviv <ohad.raviv@gmail.com>
2021-12-09 11:45:32 +0200

    added Tacho and fixed PWM wrong precision

--- smoker.ino
+++ smoker.ino
@@ -9,13 +9,32 @@
 #define SCL 14                    //Define SCL pins
 
 LiquidCrystal_I2C lcd(0x27,16,2); //initialize the LCD
+
+// tacho defs
+#define PIN_FAN_TACHO 34
+#define RPM_CALC_PERIOD 1000
+int fan1RPM = 0;
+volatile int fan1InterruptCounter;
+unsigned long previousmills;
+
+void ICACHE_RAM_ATTR handleInterruptFan1Tacho()
+{
+  fan1InterruptCounter++;
+}
+
  
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("ohad test");
-  ledcSetup(CHAN_PWM, 25000, 12);
+  Serial.println("starting..");
+  // Our fan requests freq=25Khz. ESP32's spec for precision bound us to max of 11bits.
+  // (max frequency is 80M/11=39K)
+  ledcSetup(CHAN_PWM, 25000, 8); 
   ledcAttachPin(PIN_FAN_PWM, CHAN_PWM);
+
+  pinMode(PIN_FAN_TACHO, INPUT_PULLUP);
+  attachInterrupt(PIN_FAN_TACHO, handleInterruptFan1Tacho, FALLING);
+  previousmills = millis();
 
   Wire.begin(SDA, SCL);           // attach the IIC pin
   lcd.init();                     // LCD driver initialization
@@ -27,22 +46,34 @@ void setup()
 void loop()
 {
  
-  int adcVal = analogRead(PIN_POTEN); //read adc
-  int pwmVal = adcVal;        // adcVal re-map to pwmVal
-  Serial.print("ohadohad: ");
-  Serial.println(pwmVal);
+  int adcVal = analogRead(PIN_POTEN); //read adc (12 bit)
+  int pwmVal = map(adcVal, 0, 4095, 0, 255);  // re-map to pwmVal (8 bit)
+  double potPct = adcVal / 4095.0 * 100;
+  Serial.printf("ADC Val: %d, \t PWM: %d, \t pct: %.2f\n", adcVal, pwmVal, potPct);
+  Serial.println(""); 
 
-  lcd.setCursor(0, 0);              //set the cursor to column 0, line 1
+  lcd.setCursor(0, 0);              
   char buffer[16];
-  sprintf(buffer, "ohadohad: %4d", pwmVal);
+  sprintf(buffer, "PWM pct: %3.0f%%", potPct);
   lcd.print(buffer);        //display the Humidity on the LCD1602
-  //lcd.print(pwmVal);   
-  //lcd.setCursor(0, 1);              //set the cursor to column 0, line 0 
-  //lcd.print("Humidity   :");        //display the Humidity on the LCD1602
-  //lcd.print(DHT.humidity);  
-  
-  
+
   ledcWrite(CHAN_PWM, pwmVal);    // set the pulse width.
+
+  if (millis() - previousmills > RPM_CALC_PERIOD) {
+    int elapsedMs = millis() - previousmills;
+    int count = fan1InterruptCounter;
+    fan1InterruptCounter = 0;
+    previousmills = millis();
+    
+    fan1RPM = count / 2 * elapsedMs / 1000;
+    Serial.println(fan1RPM);
+
+    lcd.setCursor(0, 1);
+    char buffer[16];
+    sprintf(buffer, "RPM: %8d", fan1RPM);
+    lcd.print(buffer);
+
+  }
  
   delay(1000);
 }
